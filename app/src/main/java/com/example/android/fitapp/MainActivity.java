70e493f8255e896999e8d6c5c7db92d357e5d881
prityusha <54525593+prityusha@users.noreply.github.com>
2021-11-17 22:54:43 +0530

    modified .gitignore

--- app/src/main/java/com/example/android/fitapp/MainActivity.java
+++ app/src/main/java/com/example/android/fitapp/MainActivity.java
@@ -16,6 +16,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -82,6 +84,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        //databaseReference.child("Latitude").push().setValue(textViewLat.getText().toString());
 
         /*loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
